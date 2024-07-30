a7ab61543a25cbb0c30c5f20c430b6b57ba2fa64
Robert Johnson <128016252+Qwer-TeX@users.noreply.github.com>
2024-07-30 19:47:14 -0400

    Added the w program to set of utilities

--- minibox.c
+++ minibox.c
@@ -33,6 +33,9 @@
 #include <libgen.h>
 #include <sys/stat.h>
 #include <sys/sysmacros.h>
+#include <utmp.h>
+#include <time.h>
+#include <sys/sysinfo.h>
 
 /* minibox specific defines */
 #define VERSION "0.1.1"
@@ -702,6 +705,62 @@ int hexdump(int argc, char *argv[]) {
     return 0;
 }
 
+/* w program */
+/* Display logged-in users and their activities */
+/* FIXME: Output is broken and doesn't display info in their correct respective columns */
+int w(void) {
+    struct sysinfo si;
+    struct utmp *ut;
+    struct tm *tm;
+    time_t now;
+    FILE *fp;
+    char buf[256];
+
+    // Get system uptime
+    if (sysinfo(&si) < 0) {
+        perror("sysinfo");
+        return 1;
+    }
+    now = time(NULL);
+
+    // Print header
+    printf(" %s up ", strtok(ctime(&now), "\n"));
+    printf("%ld users,  load average: %.2f, %.2f, %.2f\n",
+           (long)sysconf(_SC_NPROCESSORS_ONLN),
+           (double)si.loads[0] / 65536.0,
+           (double)si.loads[1] / 65536.0,
+           (double)si.loads[2] / 65536.0);
+    printf("USER     TTY      FROM              LOGIN@   IDLE   JCPU   PCPU WHAT\n");
+
+    // Read and display user information
+    setutent();
+    while ((ut = getutent())) {
+        if (ut->ut_type == USER_PROCESS) {
+            tm = localtime(&ut->ut_tv.tv_sec);
+            strftime(buf, sizeof(buf), "%H:%M", tm);
+
+            printf("%-8s %-8s %-16s %s  ", ut->ut_user, ut->ut_line, ut->ut_host[0] != '\0' ? ut->ut_host : "-", buf);
+
+            // Calculate idle time
+            snprintf(buf, sizeof(buf), "/proc/%d/stat", ut->ut_pid);
+            if ((fp = fopen(buf, "r"))) {
+                long unsigned int starttime, utime, stime;
+                if (fscanf(fp, "%*d %*s %*c %*d %*d %*d %*d %*d %*u %*u %*u %*u %*u %lu %lu %*d %*d %*d %*d %*d %*d %*d %*d %*d %*d %*u %lu", &utime, &stime, &starttime) == 3) {
+                    fclose(fp);
+                    printf("%-6s ", "IDLE");
+                    printf("%-6s ", "JCPU");
+                    printf("%-6s ", "PCPU");
+                    printf("%s\n", ut->ut_line);
+                } else {
+                    fclose(fp);
+                }
+            }
+        }
+    }
+    endutent();
+    return 0;
+}
+
 /* Update main function */
 int main(int argc, char *argv[]) {
     if (argc < 1) {
@@ -777,6 +836,8 @@ int main(int argc, char *argv[]) {
         return od(argc, argv);
     } else if (strcmp(cmd, "hexdump") == 0) {
         return hexdump(argc, argv);
+    } else if (strcmp(cmd, "w") == 0) {
+        return w();
     } else {
         printf("MiniBox %s: A multi-call binary that combines many common Unix utilities\n"
                "into one that aims to be lightweight and memory efficient.\n"
@@ -807,7 +868,8 @@ int main(int argc, char *argv[]) {
                "free:     Display memory stats (shared broken)\n"
                "xxd:      Make a hex dump\n"
                "od:       dump files in octal format\n"
-               "hexdump:  Display a file in hexadecimal\n",
+               "hexdump:  Display a file in hexadecimal\n"
+               "w:        Display info about current users on machine (output broken)\n",
                VERSION);
         return 1;
     }
