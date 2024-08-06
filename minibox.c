105a3933abcbdb22658226346372bf5abfa48f70
Robert Johnson <128016252+Qwer-TeX@users.noreply.github.com>
2024-08-06 18:20:14 -0400

    Add the cut command

--- minibox.c
+++ minibox.c
@@ -804,6 +804,142 @@ int vmstat() {
   return 0;
 }
 
+int cut(int argc, char *argv[]) {
+  char delimiter = '\t';  // Default delimiter (tab)
+  char *field_str = NULL; // Field string
+  int opt;
+  size_t num_fields = 0;
+  int *fields = NULL;
+
+  // Parse command-line options
+  while ((opt = getopt(argc, argv, "d:f:")) != -1) {
+    switch (opt) {
+    case 'd':
+      delimiter = optarg[0];
+      break;
+    case 'f':
+      field_str = optarg;
+      break;
+    default:
+      fprintf(stderr, "Usage: %s [-d delimiter] -f fields [file...]\n",
+              argv[0]);
+      return EXIT_FAILURE;
+    }
+  }
+
+  if (!field_str) {
+    fprintf(stderr, "Field specification is required.\n");
+    return EXIT_FAILURE;
+  }
+
+  // Parse the field argument
+  size_t fields_capacity = 10;
+  fields = malloc(fields_capacity * sizeof(int));
+  if (!fields) {
+    perror("malloc");
+    return EXIT_FAILURE;
+  }
+
+  char *str = strdup(field_str);
+  if (!str) {
+    perror("strdup");
+    free(fields);
+    return EXIT_FAILURE;
+  }
+
+  char *token = strtok(str, ",");
+  while (token != NULL) {
+    if (num_fields >= fields_capacity) {
+      fields_capacity *= 2;
+      fields = realloc(fields, fields_capacity * sizeof(int));
+      if (!fields) {
+        perror("realloc");
+        free(str);
+        return EXIT_FAILURE;
+      }
+    }
+    fields[num_fields++] = atoi(token);
+    token = strtok(NULL, ",");
+  }
+  free(str);
+
+  // Process lines from stdin or files
+  if (optind == argc) {
+    // No files specified, read from stdin
+    char line[1024];
+    while (fgets(line, sizeof(line), stdin)) {
+      char *temp_line = strdup(line);
+      if (!temp_line) {
+        perror("strdup");
+        free(fields);
+        return EXIT_FAILURE;
+      }
+
+      char *token = strtok(temp_line, &delimiter);
+      int current_field = 1;
+      size_t field_index = 0;
+
+      while (token != NULL) {
+        while (field_index < num_fields &&
+               fields[field_index] < current_field) {
+          field_index++;
+        }
+        if (field_index < num_fields && fields[field_index] == current_field) {
+          printf("%s\n", token);
+        }
+        current_field++;
+        token = strtok(NULL, &delimiter);
+      }
+
+      free(temp_line);
+    }
+  } else {
+    // Process each file
+    for (int i = optind; i < argc; i++) {
+      FILE *file = fopen(argv[i], "r");
+      if (!file) {
+        perror(argv[i]);
+        continue;
+      }
+
+      char line[1024];
+      while (fgets(line, sizeof(line), file)) {
+        char *temp_line = strdup(line);
+        if (!temp_line) {
+          perror("strdup");
+          fclose(file);
+          free(fields);
+          return EXIT_FAILURE;
+        }
+
+        char *token = strtok(temp_line, &delimiter);
+        int current_field = 1;
+        size_t field_index = 0;
+
+        while (token != NULL) {
+          while (field_index < num_fields &&
+                 fields[field_index] < current_field) {
+            field_index++;
+          }
+          if (field_index < num_fields &&
+              fields[field_index] == current_field) {
+            printf("%s\n", token);
+          }
+          current_field++;
+          token = strtok(NULL, &delimiter);
+        }
+
+        free(temp_line);
+      }
+
+      fclose(file);
+    }
+  }
+
+  free(fields);
+  return EXIT_SUCCESS;
+}
+
 /* Update main function */
 int main(int argc, char *argv[]) {
   if (argc < 1) {
@@ -883,6 +1019,8 @@ int main(int argc, char *argv[]) {
     return w();
   } else if (strcmp(cmd, "vmstat") == 0) {
     return vmstat();
+  } else if (strcmp(cmd, "cut") == 0) {
+    return cut(argc, argv);
   } else {
     printf("MiniBox %s: A multi-call binary that combines many common Unix "
            "utilities\n"
@@ -919,7 +1057,8 @@ int main(int argc, char *argv[]) {
            "hexdump:  Display a file in hexadecimal\n"
            "w:        Display info about current users on machine (output "
            "broken)\n"
-           "vmstat:   Report virtual memory statistics (output broken)\n",
+           "vmstat:   Report virtual memory statistics (output broken)\n"
+           "cut:      exclude sections of lines in files and print to stdout\n",
            VERSION);
     return 1;
   }
