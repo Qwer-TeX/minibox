9f1a1320287f7ddbfaa3ba395484502f4383b382
Robert Johnson <128016252+Qwer-TeX@users.noreply.github.com>
2024-08-31 10:47:30 -0400

    Add the libmb sources

---
+++ libmb/xmalloc.c
@@ -0,0 +1,30 @@
+/* MiniBox is a busybox/toybox like replacement aiming to be lightweight,
+ * portable, and memory efficient.
+ *
+ * Copyright (C) 2024 Robert Johnson et al <mitnew842@gmail.com>.
+ * All Rights Reserved.
+ *
+ * Licensed under Unlicense License, see file LICENSE in this source tree.
+ *
+ * When adding programs or features, please consider if they can be
+ * accomplished in a sane way with standard unix tools. If they're
+ * programs or features you added, please make sure they are read-
+ * able and understandable by a novice-advanced programmer, if not,
+ * add comments or let me know. Use common sense and please don't
+ * bloat sources.
+ *
+ * I haven't tested but it could compile on windows systems with MSYS/MinGW or
+ * Cygwin. MiniBox should be fairly portable for POSIX systems.
+ *
+ * Licensed under Unlicense License, see file LICENSE in this source tree.
+ */
+#include "libmb.h"
+
+void *xmalloc(size_t size) {
+  void *ptr = malloc(size);
+
+  if (ptr == NULL)
+    abort();
+  else
+    return ptr;
+}
